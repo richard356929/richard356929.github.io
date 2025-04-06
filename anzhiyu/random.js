c39f0be235f7803cf2b746e5ab04dcfbd8dfb897
qifei <623218056@qq.com>
2025-04-06 22:56:07 +0800

    Site updated: 2025-04-06 22:56:07

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/02/1-Docker是什么？/"];function toRandomPost(){
+var posts=["2025/04/06/Chrome浏览器多开，账号独立运行/","2025/04/02/1-Docker是什么？/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
