13207149387ff9ecaa4241c8ceb95ebe2fd706e5
qifei <623218056@qq.com>
2025-04-09 22:08:37 +0800

    Site updated: 2025-04-09 22:08:37

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/07/网站配置google-Analytics/","2025/04/07/网站配置google-search-console/","2025/04/07/hexo博客站点地图配置/","2025/04/07/Linux文本搜索利器：grep命令详解与实战指南/","2025/04/06/hexo博客push到github报权限错误/","2025/04/06/Chrome浏览器多开，账号独立运行/","2025/04/02/1-Docker是什么？/"];function toRandomPost(){
+var posts=["2025/04/09/超强大的-Nginx-可视化管理平台-Nginx-Proxy-Manager/","2025/04/07/网站配置google-Analytics/","2025/04/07/网站配置google-search-console/","2025/04/07/hexo博客站点地图配置/","2025/04/07/Linux文本搜索利器：grep命令详解与实战指南/","2025/04/06/hexo博客push到github报权限错误/","2025/04/06/Chrome浏览器多开，账号独立运行/","2025/04/02/1-Docker是什么？/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
